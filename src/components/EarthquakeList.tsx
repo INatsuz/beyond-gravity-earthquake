f95ab525257edc110a1cee1d6d71f366fe5ce724
Vasco <vascoraminhos@hotmail.com>
2025-08-30 14:32:42 +0300

    Added header to list Added node modules to gitignore

--- src/components/EarthquakeList.tsx
+++ src/components/EarthquakeList.tsx
@@ -10,16 +10,25 @@ type EarthquakeListProps = {
 
 export default function EarthquakeList(props: EarthquakeListProps) {
 	return (
-		<ul className="earthquake-list h-100 p-0 overflow-y-scroll">
-			{
-				props.earthquakes && props.earthquakes.map((item) => (<EarthquakeListItem item={item} onClick={props.onClick} isActive={item.id === props.activeEarthquake} />))
-			}
+		<>
 
-			{
-				props.earthquakes &&
-				props.earthquakes.length < 200 &&
-				<li className="border-bottom text-center" role="button" onClick={() => props.loadMore()}>Click to Load More</li>
-			}
-		</ul>
+			<ul className="earthquake-list h-100 p-0 overflow-y-scroll">
+				<li>
+					<div className="border-bottom py-4 bg-light">
+						<h5 className="text-center">Recent Earthquakes</h5>
+						<p className="text-center m-0">Showing {props.earthquakes?.length}</p>
+					</div>
+				</li>
+				{
+					props.earthquakes && props.earthquakes.map((item) => (<EarthquakeListItem item={item} onClick={props.onClick} isActive={item.id === props.activeEarthquake}/>))
+				}
+
+				{
+					props.earthquakes &&
+					props.earthquakes.length < 200 &&
+					<li className="border-bottom text-center" role="button" onClick={() => props.loadMore()}>Click to Load More</li>
+				}
+			</ul>
+		</>
 	);
 }
